e67e7b9f6ea1049865f2797abeb41b5805c28210
Ahileshveena <52243973+Ahileshveena@users.noreply.github.com>
2019-06-26 17:39:13 +0530

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,7 @@
+num1=input('enter the first number:')
+"""its required the first number"""
+num2=input('enter the second number:')
+"""its required the second number"""
+sum=float(num1)*float(num2)
+"""sum helps to add the first number and second number"""
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
